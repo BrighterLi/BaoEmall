402631b17c94fc58d5a17b8968fa4c499f60f749
liming <287055586@qq.com>
2020-02-24 23:51:15 +0800

    由于底部导航有5个部分，重复了5次，所以进行抽离封装，添加Tab数据类

--- app/src/main/java/com/xiaoming/baoemall/MainActivity.java
+++ app/src/main/java/com/xiaoming/baoemall/MainActivity.java
@@ -6,25 +6,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.xiaoming.baoemall.bean.Tab;
+import com.xiaoming.baoemall.fragment.CategoryFragment;
 import com.xiaoming.baoemall.fragment.HomeFragment;
 import com.xiaoming.baoemall.widget.FragmentTabHost;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private FragmentTabHost mTabHost;
     private LayoutInflater mLInflater;
+
+    private List<Tab> mTabList = new ArrayList<>(5);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLInflater = LayoutInflater.from(this);
+        initTab();
+
+        //这部分也可以放进initTab()，逻辑更清晰简洁
+        /*mLInflater = LayoutInflater.from(this);
 
         mTabHost = (FragmentTabHost) findViewById(R.id.tab_host);
-        mTabHost.setup(this,getSupportFragmentManager(),R.id.real_tab_content);
+        mTabHost.setup(this,getSupportFragmentManager(),R.id.real_tab_content);*/
 
-        FragmentTabHost.TabSpec tabSpec = mTabHost.newTabSpec("home");
+        //由于有5个tab,需要重复5次，故将该部分进行封装。增加一个Tab类，将变化部分放进去
+        /*FragmentTabHost.TabSpec tabSpec = mTabHost.newTabSpec("home");
         View view = mLInflater.inflate(R.layout.tab_indicator, null);
         ImageView image = view.findViewById(R.id.icon_tab);
         TextView text = view.findViewById(R.id.text_indicator);
@@ -32,6 +44,56 @@ public class MainActivity extends AppCompatActivity {
         text.setText("主页");
         tabSpec.setIndicator(view);
 
-        mTabHost.addTab(tabSpec, HomeFragment.class,null);
+        mTabHost.addTab(tabSpec, HomeFragment.class,null);*/
+    }
+
+    //https://www.jianshu.com/p/4d4a83945193
+    private void initTab() {
+        Tab tabHome = new Tab(HomeFragment.class, R.string.home,R.mipmap.icon_home);
+        Tab tabHot = new Tab(HomeFragment.class, R.string.hot,R.mipmap.icon_hot);
+        Tab tabCategory = new Tab(CategoryFragment.class, R.string.category,R.mipmap.icon_discover);
+        Tab tabCart = new Tab(HomeFragment.class, R.string.cart,R.mipmap.icon_cart);
+        Tab tabMine = new Tab(HomeFragment.class, R.string.mine,R.mipmap.icon_user);
+
+        mTabList.add(tabHome);
+        mTabList.add(tabHot);
+        mTabList.add(tabCategory);
+        mTabList.add(tabCart);
+        mTabList.add(tabMine);
+
+        mLInflater = LayoutInflater.from(this);
+        //TabHost
+        mTabHost = findViewById(R.id.tab_host);
+        mTabHost.setup(this, getSupportFragmentManager(),R.id.real_tab_content);
+
+        //添加TabSpec了，有几个Tab，就添加几个TabSpec
+        for (Tab tab : mTabList) {
+            //TabSpec
+            FragmentTabHost.TabSpec tabSpec = mTabHost.newTabSpec(getString(tab.getTitle()));
+            //Indicator
+            tabSpec.setIndicator(buildIndicator(tab));
+            //TabHost
+            mTabHost.addTab(tabSpec,tab.getFragment(),null);
+
+        }
+
+        //加分隔线
+        //mTabHost.getTabWidget().setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+        //去掉分隔线
+        mTabHost.getTabWidget().setShowDividers(LinearLayout.SHOW_DIVIDER_NONE);
+        //设置默认显示第一个
+        mTabHost.setCurrentTab(0);
+    }
+
+    //Indicator包含TextView和ImageView
+    private View buildIndicator(Tab tab) {
+        View view = mLInflater.inflate(R.layout.tab_indicator,null);
+        ImageView img = view.findViewById(R.id.icon_tab);
+        TextView text = view.findViewById(R.id.text_indicator);
+
+        img.setBackgroundResource(tab.getIcon());
+        text.setText(tab.getTitle());
+
+        return view;
     }
 }
