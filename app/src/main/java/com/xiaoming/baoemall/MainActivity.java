6b3d60a24b96874698a02c92f71e0b298a388c72
liming <287055586@qq.com>
2020-02-24 08:43:00 +0800

    用FragmentTabHost实现底部菜单

--- app/src/main/java/com/xiaoming/baoemall/MainActivity.java
+++ app/src/main/java/com/xiaoming/baoemall/MainActivity.java
@@ -2,12 +2,36 @@ package com.xiaoming.baoemall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.xiaoming.baoemall.fragment.HomeFragment;
+import com.xiaoming.baoemall.widget.FragmentTabHost;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentTabHost mTabHost;
+    private LayoutInflater mLInflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLInflater = LayoutInflater.from(this);
+
+        mTabHost = (FragmentTabHost) findViewById(R.id.tab_host);
+        mTabHost.setup(this,getSupportFragmentManager(),R.id.real_tab_content);
+
+        FragmentTabHost.TabSpec tabSpec = mTabHost.newTabSpec("home");
+        View view = mLInflater.inflate(R.layout.tab_indicator, null);
+        ImageView image = view.findViewById(R.id.icon_tab);
+        TextView text = view.findViewById(R.id.text_indicator);
+        image.setBackgroundResource(R.mipmap.icon_home);
+        text.setText("主页");
+        tabSpec.setIndicator(view);
+
+        mTabHost.addTab(tabSpec, HomeFragment.class,null);
     }
 }
---
+++ readme.txt
@@ -0,0 +1,16 @@
+一 整体架构
+1 整体架构
+  (1) 用FragmentTabHost实现底部菜单
+  (2) ToolBar的基本使用
+  (3) 自定义ToolBar
+
+2 首页
+
+二 知识内容
+1 用FragmentTabHost实现底部菜单
+  (1) FragmentTabHost的用法
+    1) Activity 要继承FragmentActivity
+    2) 调用setup()方法
+    3) 添加TabSpec
+  (2) Selector背景选择器
+  (3) 底部菜单的几种实现方式
